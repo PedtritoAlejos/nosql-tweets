dac5c1d256c277df45573b853a7b09f4bf322bf2
pealejosb <pedroalejosb@gmail.com>
2020-09-28 10:41:58 -0300

    Se agrega mensaje de salida de las pruebas

--- app.js
+++ app.js
@@ -35,23 +35,17 @@ db();
 
 let email_time = `user_${moment().format("YYYYMMDDhmmss")}@gmail.com `;
 
-
+let usuario = {
+    name : 'goku',
+    surnames:'Son',
+    password:'123456789',
+    email:email_time,
+    avatar:'https://i.pinimg.com/564x/a4/f7/ac/a4f7acf38a8e618a4371f4c86c68c8c2.jpg'
+};
 
 // Insertar un usuario de ejemplo
-let usuarioNuevo = new usermodel({
-    name : 'goku',
-    surnames:'lopez perez',
-    password:'123456789',
-    email:email_time,
-    avatar:'goku.jpg'
-});
-let usuarioRepetido = new usermodel({
-    name : 'goku',
-    surnames:'lopez perez',
-    password:'123456789',
-    email:email_time,
-    avatar:'goku.jpg'
-});
+let usuarioNuevo = new usermodel(usuario);
+let usuarioRepetido = new usermodel(usuario);
 
 
 
@@ -60,12 +54,33 @@ usuarioNuevo.save( (error,usuarioDB) => {
     console.log( chalk.white("***************************************************************") );
     console.log( chalk.yellow(`---- INGRESANDO USUARIO CON E-MAIL UNICO (${email_time}) ---`) );
     console.log( chalk.white("***************************************************************") );
-
+    console.log("USUARIO A CREAR",usuario);
     if(error){
         console.log("error ",error.errmsg);
       
     }else{
-        console.log(usuarioDB);
+        
+        console.log("USUARIO DB :",usuarioDB);
+        
+     
+        let tweetNuevo = new tweetmodel(
+            {
+                userId:usuarioDB,
+                message:'¡Hola, mi nombre es Goku! '
+            }
+        );
+
+        tweetNuevo.save( (error,tweetDB) =>{
+            if(error){
+                console.log(error.errmsg);
+            }else{
+                console.log( chalk.white("***************************************************************") );
+                console.log( chalk.yellow(`---- TWEET INGRESADO DEL USUARIO CREADO CON ID (${usuarioDB._id}) ---`) );
+                console.log( chalk.white("***************************************************************") );
+                console.log(tweetDB);
+                console.log( chalk.white("***************************************************************") );
+            }
+        } );
     }
     console.log( chalk.white("***************************************************************") );
 } ); 
@@ -82,7 +97,7 @@ usuarioRepetido.save( (error,usuarioDB) => {
         console.log(usuarioDB);
     }
     console.log( chalk.white("***************************************************************") );
-} );
+}); 
 //
 
 
@@ -91,4 +106,4 @@ const port = process.env.PORT || 8000;
 
 app.listen(port,() =>{
     console.log(`Server corrienedo en puerto : ${port}`);
-})
+});
